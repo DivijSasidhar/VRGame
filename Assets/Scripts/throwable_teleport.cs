0c6437d705eb60ba9b510f00d1d3b2256c58dccd
Tolga Cavusoglu <24cavtol@hawken.edu>
2023-12-14 00:47:03 -0500

    More level design
    we need to scrap the finale, there is no way we can get all of that working in time. Light puzzle is also doing weird things, might need to scrap that as well.

---
+++ Assets/Scripts/throwable_teleport.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class throwable_teleport : MonoBehaviour
+{
+    [SerializeField] Vector3 start = new Vector3(0, 0, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Teleport()
+    {
+        transform.position = start;
+    }
+}
---
+++ Assets/Scripts/throwablesdoor.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class throwablesdoor : MonoBehaviour
+{
+    int[] triggers = new int[] { 0, 0, 0, 0, 0, 0, 0, 0};
+
+    [SerializeField] Vector3 origin = new Vector3(0, 0, 0);
+    bool goingPointA = false;
+    [SerializeField] Vector3 destination = new Vector3(0, 3, 0);
+    [SerializeField] float speed = 1;
+    float t;
+    bool success;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((goingPointA) & (!success))
+        {
+            t += Time.deltaTime * speed;
+            transform.position = Vector3.Lerp(origin, destination, t);
+        }
+        if (t >= 1)
+        {
+            goingPointA = false;
+            success = true;
+            t = 0;
+        }
+    }
+
+
+    void TargetUpdate(int target_number)
+    {
+        triggers[target_number] = 1;
+        int sum = 0;
+        for (int i = 0; i < triggers.Length; i++)
+        {
+            sum += triggers[i];
+        }
+
+        if (sum == 8)
+        {
+            goingPointA = true;
+        } 
+
+    }
+}
